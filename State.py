760e3387ab881e93002432291ad9c9c30e6a53aa
qym7 <876444960@qq.com>
2021-02-21 17:34:38 +0100

    single window plot

--- State.py
+++ State.py
@@ -5,7 +5,15 @@ from Ball import Ball, Position, Velocity
 
 class State(object):
     
-    def __init__(self, screen_x, screen_y, balls, endline, bg_color=(0, 165, 255)):
+    def __init__(self,
+                 screen_x,
+                 screen_y,
+                 endline,
+                 balls,
+                 score=0,
+                 is_begin=True,
+                 is_final=False,
+                 bg_color=(0, 165, 255)):
         """
         screen_x: float, the width of the screen
         screen_y: float, the height of the screen
@@ -16,26 +24,43 @@ class State(object):
         assert endline < screen_y
         self.endline = endline
         self.balls = balls
+        self.score = score
+        self.is_begin = is_begin
+        self.is_final = is_final
         self.bg_color = bg_color
-        self.canvas = InitCanvas(self.screen_x, self.screen_y, color=bg_color)
-
-    def reset_canvas(self):
         self.canvas = InitCanvas(self.screen_x, self.screen_y, color=self.bg_color)
+        self.step = 0
 
-    def plot_state(self):
+    def plot_state(self, is_save=False, path="./result/", speed=300):
         """Plot the State with an image"""
+        if not self.is_begin:
+            self.is_begin = True
+            cv.namedWindow('state', cv.WINDOW_NORMAL)
+
         cv.line(self.canvas,
                 (0, self.screen_y - self.endline),
                 (self.screen_x, self.screen_y - self.endline),
                 (0, 0, 255), thickness=2)
+        cv.putText(self.canvas, "Score : {}".format(self.score), (40, 50), cv.FONT_HERSHEY_PLAIN, 2.0, (0, 0, 255), 2)
         for ball in self.balls:
             cv.circle(self.canvas,
-                      (ball.position.x, self.screen_y-ball.position.y),
+                      (ball.position[0], self.screen_y-ball.position[1]),
                       ball.radius, ball.color, -1)
 
         cv.imshow('state', self.canvas)
-        cv.waitKey(0)
-        cv.destroyWindow('state')
+        if is_save:
+            cv.imwrite((path+"{}.jpg").format(self.step), img=self.canvas)
+            print("saved")
+        if self.is_final:
+            cv.waitKey(0)
+            cv.destroyWindow('state')
+        cv.waitKey(speed)
+
+        return self.canvas
+
+    def video(self):
+        """TODO: Save the video"""
+        pass
 
 
 def InitCanvas(width, height, color=(255, 255, 255)):
--- test.py
+++ test.py
@@ -2,17 +2,22 @@ def test_state_plot():
     from State import State
     from Ball import Ball, Velocity, Position
 
-    balls = []
     ball_nbr = 10
     x = 300
     y = 500
     endline = 0.8
     radius = int(x/ball_nbr/2)
-    for i in range(ball_nbr):
-        balls.append(Ball(Position(int(x*i/ball_nbr) + radius, radius),
-                          Velocity(0, 0),
-                          radius=radius,
-                          color=(255, 0, 100)))
-
-    state = State(x, y, balls, int(endline*y))
-    state.plot_state()
+    T = 10
+    for t in range(T):
+        balls = []
+        for i in range(ball_nbr):
+            balls.append(Ball(Position(int(x*i/ball_nbr) + radius, radius + t*10),
+                              Velocity(0, 0),
+                              radius=radius,
+                              color=(255, 0, 100)))
+        state = State(x, y, int(endline * y), balls=balls)
+        state.step = t
+        state.balls = balls
+        if t == T-1:
+            state.is_final = True
+        state.plot_state(is_save=True)
